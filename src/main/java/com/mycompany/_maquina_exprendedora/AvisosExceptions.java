ab9054bd4e7bc81342a61696f430a0f24cf0c1ca
user <user@PC514>
2020-11-18 11:59:52 +0100

    Push de todo 4

---
+++ src/main/java/com/mycompany/_maquina_exprendedora/AvisosExceptions.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany._maquina_exprendedora;
+
+/**
+ *
+ * @author user
+ */
+class AvisosExceptions extends Exception {
+
+    public AvisosExceptions(String msg) {
+        super(msg);
+    }
+    
+}
